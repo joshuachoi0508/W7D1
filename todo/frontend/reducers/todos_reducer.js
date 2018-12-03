2ada0256049af783bc75b4f9b22b82fc4d8ff1ee
joshuachoi0508 <joshuachoi0508@gmail.com>
2018-12-03 15:05:03 -0800

    Implement some of todo actions, reducers, and store

---
+++ todo/frontend/reducers/todos_reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case RECEIVE_TODOS:
+      return ;
+    case RECEIVE_TODO:
+      return action.todo;
+    default:
+      return state;
+  }
+};
+
+
+
+
+
+export default todosReducer;
